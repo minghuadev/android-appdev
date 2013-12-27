8e5c6b7974354b38eba59e4d1618d789786f8a86
minghua <minghua@localhost>
2013-12-26 21:31:09 -0800

    implement new activity

--- devexample01/src/com/example/myfirstprj/DisplayMessageActivity.java
+++ devexample01/src/com/example/myfirstprj/DisplayMessageActivity.java
@@ -2,10 +2,11 @@ package com.example.myfirstprj;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 
 public class DisplayMessageActivity extends Activity {
@@ -13,7 +14,20 @@ public class DisplayMessageActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_display_message);
+		////setContentView(R.layout.activity_display_message);
+		
+	    // Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    textView.setText(message);
+
+	    // Set the text view as the activity layout
+	    setContentView(textView);
+		
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
@@ -26,13 +40,6 @@ public class DisplayMessageActivity extends Activity {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.display_message, menu);
-		return true;
 	}
 
 	@Override
