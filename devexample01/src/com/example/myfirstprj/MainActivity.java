75fb62ab117f6ae70ecefb9e8968d89023dee2ab
minghua <minghua@localhost>
2013-12-26 22:25:22 -0800

    change main activity title. bump up version. start 2nd activity.

--- devexample01/src/com/example/myfirstprj/MainActivity.java
+++ devexample01/src/com/example/myfirstprj/MainActivity.java
@@ -29,7 +29,8 @@ public class MainActivity extends Activity {
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
     	EditText editText = (EditText) findViewById(R.id.edit_message);
     	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);    	
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
     }
     
 }
