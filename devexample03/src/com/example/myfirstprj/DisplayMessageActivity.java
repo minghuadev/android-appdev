1ce4e7a6242003ba5c65c9dd5cb39bbb14b084b1
minghua <minghua@localhost>
2013-12-28 21:45:29 -0800

    v1.06 retrieve and show info from net.

--- devexample03/src/com/example/myfirstprj/DisplayMessageActivity.java
+++ devexample03/src/com/example/myfirstprj/DisplayMessageActivity.java
@@ -1,6 +1,14 @@
 package com.example.myfirstprj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,12 +20,21 @@ import android.os.Build;
 
 public class DisplayMessageActivity extends Activity {
 	
+	static final private String passcode = "password";
+	static final private String cardcode = "LZ0GjL36";
+	
 	private EditText _txtview = null;
+	private boolean  _allownet = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_message);
+
+		// Allow network blocking access... change api min from 8 to 9
+		  StrictMode.ThreadPolicy policy = new StrictMode.
+				  ThreadPolicy.Builder().permitAll().build();
+				  StrictMode.setThreadPolicy(policy);
 		
 	    // Get the message from the intent
 	    Intent intent = getIntent();
@@ -39,6 +56,9 @@ public class DisplayMessageActivity extends Activity {
 	        
 	    }
 	    _txtview.setText(message);
+	    if ( message.contains(passcode) ) {
+	    	_allownet = true;
+	    }
 
 	    // Set the text view as the activity layout
 	    //setContentView(_txtview);
@@ -81,6 +101,14 @@ public class DisplayMessageActivity extends Activity {
             //openSearch();
         	if ( _txtview != null ) {
         		_txtview.setText( _txtview.getText() + " search ");
+        		if ( _allownet ) {
+	        		Netreq nrq = new Netreq();
+	        		String result = nrq.getData();
+	        		_txtview.setText( _txtview.getText() + "\n\n" + result + "\n");
+        		} else {
+	        		_txtview.setText( _txtview.getText() + "\n\n" + 
+	        							"No net access allowed" + "\n");
+        		}
         	}
             return true;
         case R.id.action_settings:
@@ -93,6 +121,103 @@ public class DisplayMessageActivity extends Activity {
         	break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	public class Netreq {
+
+	    /**
+	     * @param args the command line arguments
+	     */
+	    public void main(String[] args) {
+	        Netreq nrq = new Netreq();
+	        String s = nrq.getData();
+	        System.out.printf("%s\n",s);
+	    }
+	    
+	    private String getData() {
+	        String site2 = "trello";
+	/* https://trello.com/c/LZ0GjL36/27-making-requests-to-the-api-i-e-api-trello-com */
+	        String card3 = cardcode;
+	        String site = "https://api." + site2 + ".com/1/cards/" + card3 + "/desc";
+	        String rs = requestLine(site);
+	        String s1 = "{\"_value\":\"";
+	        String s2 = "\"}";
+	        StringBuilder retsb = new StringBuilder();
+	        if ( rs.toLowerCase().startsWith("error:") ) {
+	            retsb.append(rs);
+	        } else if (rs.startsWith(s1)  && rs.endsWith(s2)) {
+	            String body2 = rs.substring( s1.length() );
+	            String body = body2.substring(0, body2.length()-s2.length());
+	            int len = body.length();
+	            for (int i=0; i<len; i++) {
+	                char c = body.charAt(i);
+	                if ( c == '\\' && i < len-1 && body.charAt(i+1) == 'n' ) {
+	                    retsb.append("\n\n");
+	                    i++;
+	                } else {
+	                    retsb.append(c);
+	                }
+	            }
+	        } else {
+	            retsb.append("Error: Unknown string format: ").append(rs);
+	        }
+	        return retsb.toString();
+	    }
+	    
+	    private String requestLine(String site) {
+	        String dbgstr = null;
+	        String retstr = "";
+	        try {
+	            URL url = new URL(site);
+	            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+	            retstr = readStreamLines(con.getInputStream());
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            dbgstr = "Error: url connection or stream exception\n";
+	        }
+	        if (dbgstr == null) {
+	            return retstr;
+	        } else {
+	            return dbgstr;
+	        }
+	    }
+
+	    private String readStreamLines(InputStream in) {
+	        String dbgstr = null;
+	        StringBuilder retsb = new StringBuilder();
+	        BufferedReader reader = null;
+	        try {
+	            reader = new BufferedReader(new InputStreamReader(in));
+	            String line = "";
+	            while ((line = reader.readLine()) != null) {
+	                retsb.append(line); //System.out.println(line);
+	            }
+	            /* {"_value":"   7157  3 3    \n   7158  0 0"} */
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            dbgstr = "Error: reader readline exception\n";
+	        } finally {
+	            if (reader != null) {
+	                try {
+	                    reader.close();
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                    if ( dbgstr != null ) {
+	                        dbgstr += "Error: reader close exception\n";
+	                    } else {
+	                        dbgstr = "Error: reader close exception\n";
+	                    }
+	                }
+	            }
+	        }
+	        if ( dbgstr == null ) {
+	            return retsb.toString();
+	        } else {
+	            return dbgstr;
+	        }
+	    } 
 	}
 
 }
